fdae527e55d5bbc4edd9f110e7a995d3d62445ee
statova-nadejda <statovanadejda04@gmail.com>
2026-02-10 20:32:34 +0200

    add lab01 and lab02

---
+++ php/php_lab02/index.php
@@ -0,0 +1,10 @@
+<?php
+
+echo "Hello, World with echo!\n";
+print "Hello, World with print!\n";
+
+$days = 288;
+$message = "Все возвращаются на работу";
+
+echo $message . " в течении " . $days . " дней!<br> ";
+echo "{$message} в течении {$days} дней\n";
